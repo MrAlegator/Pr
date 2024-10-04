d81fcbdc0cfaccdac71fcb399337eb038d445066
MrAlegator <128161719+MrAlegator@users.noreply.github.com>
2024-10-04 16:44:32 +0300

    Update Test.sh

--- Test.sh
+++ Test.sh
@@ -1,1 +1,16 @@
+#!/bin/bash
 
+start() {
+    # Print "Hello"
+    echo "Hello"
+
+    # Prompt user for input
+    read -p "Enter a number: " input
+
+    # Check if the input is 1
+    if [ "$input" -eq 1 ]; then
+        echo "bye"
+    fi
+}
+
+start
