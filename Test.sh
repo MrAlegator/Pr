a65b549123f186298f6038dcaf007c96e60f1789
MrAlegator <128161719+MrAlegator@users.noreply.github.com>
2024-10-04 19:40:15 +0300

    Update Test.sh

--- Test.sh
+++ Test.sh
@@ -41,7 +41,7 @@ else
     wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb -P /tmp
     sudo apt install --assume-yes /tmp/chrome-remote-desktop_current_amd64.deb
     echo "Starting Chrome Remote Desktop host..."
-    eval $command
+    eval "$command"
     sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'
     echo "export CHROME_REMOTE_DESKTOP_DEFAULT_DESKTOP_SIZES=1920x1080,1080x720" >> ~/.profile
 
