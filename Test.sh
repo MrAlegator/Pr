07fe1fef4b3b4643fb12b04c27fc9872cd749737
MrAlegator <128161719+MrAlegator@users.noreply.github.com>
2024-10-04 17:10:15 +0300

    Update Test.sh

--- Test.sh
+++ Test.sh
@@ -1,28 +1,23 @@
 #!/bin/bash
 
-start() {
-    # Print "Hello"
+rx-start() Pkg   # Print "Hello"
     echo "
-██████╗░███████╗███╗░░░███╗░█████╗░████████╗██╗
-██╔══██╗██╔════╝████╗░████║██╔══██╗╚══██╔══╝██║
-██████╔╝█████╗░░██╔████╔██║██║░░██║░░░██║░░░██║
-██╔══██╗██╔══╝░░██║╚██╔╝██║██║░░██║░░░██║░░░██║
-██║░░██║███████╗██║░╚═╝░██║╚█████╔╝░░░██║░░░██║
-╚═╝░░╚═╝╚══════╝╚═╝░░░░░╚═╝░╚════╝░░░░╚═╝░░░╚═╝
-██╗░░██╗
-╚██╗██╔╝
-░╚███╔╝░
-░██╔██╗░
-██╔╝╚██╗
-╚═╝░░╚═╝"
+██████╗░██╗░░██╗
+██╔══██╗╚██╗██╔╝
+██████╔╝░╚███╔╝░
+██╔══██╗░██╔██╗░
+██║░░██║██╔╝╚██╗
+╚═╝░░╚═╝╚═╝░░╚═╝"
+    echo "Remotix - A utility that allows you to install various add-ons in linux"
+    echo: "1 - Linux (Virtual-Machine)                       3 - Linux (SSH-Port)"
+    echo: "2 - Linux (Parallel-Space)                        4 - Linux (Pkg-Codespace)"
 
-    # Prompt user for input
-    read -p "Enter a number: " input
+    read -p "Select the desired option:" input
 
     # Check if the input is 1
     if [ "$input" -eq 1 ]; then
-        echo "bye"
+        echo "1"
     fi
 }
 
-start
+rx-start
